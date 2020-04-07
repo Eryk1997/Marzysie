89e1f1586d5d8b15fe509650cd1226b7e7f88c74
Eryk1997 <erykmati@o2.pl>
2020-04-07 18:20:28 +0200

    create footer

---
+++ src/components/Footer.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className="container-flex bg-dark mt-5">
+        <div className="row" >
+            <ul className="col-2 ml-4 list-unstyled text-white">
+                <li>x</li>
+                <li>x</li>
+                <li>x</li>
+            </ul>
+            <ul className="col-2 ml-4 list-unstyled text-white">
+                <li>x</li>
+                <li>x</li>
+                <li>x</li>
+            </ul>
+            <ul className="col-2 ml-4 list-unstyled text-white">
+                <li>x</li>
+                <li>x</li>
+                <li>x</li>
+            </ul>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Footer;
