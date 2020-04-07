cea7f0b7b2eec3373b9f89c5b9669241aa473fa2
Eryk1997 <erykmati@o2.pl>
2020-04-07 18:19:57 +0200

    add footer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 import Home from './pages/Home/Home';
 import Frame from './pages/Home/Frame';
 
+
 function App() {
   return (
-    <div className="bg-dark">
+    <div className="bg-secondary ">
       <Navbar />
       <Home />
+      <Footer />
     </div>
   );
 }
