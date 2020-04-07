8e963ea27f9fda8c61e5305d8c884a26889789fa
Eryk1997 <erykmati@o2.pl>
2020-04-07 17:55:53 +0200

    create example of frame

---
+++ src/pages/Home/Frame.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import Foto from './img/260141.jpg';
+
+class Frame extends Component {
+  render() {
+    return (
+      <div className="container mt-5 ml-5 mr-5">
+        <div className="card" style={{width: "18rem"}}>
+          <img className="card-img-top" src={Foto} alt="Card image cap" />
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+            <a href="#" className="btn btn-primary">
+              Go somewhere
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Frame;
